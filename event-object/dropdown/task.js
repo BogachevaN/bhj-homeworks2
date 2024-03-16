29cc88254da47f2a51b0c4bf6da03c1b9cae8f27
Natali Bogacheva <npbogacheva@gmail.com>
2024-03-16 19:19:22 +0300

    add ddropdown value

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,23 +1,14 @@
 const list = document.querySelector('.dropdown__list')
 
-function openMenu () {
-    list.classList.add('dropdown__list_active')  
-}
+list.closest('div').addEventListener('click', () => {
+    list.classList.add('dropdown__list_active') 
+})
 
-function closeMenu () {
-    list.classList.remove('dropdown__list_active')  
-}
-
-const clickEl = document.querySelector('.dropdown')
-
-list.className.includes('dropdown__list_active')
-? clickEl.addEventListener('click', closeMenu)
-: clickEl.addEventListener('click', openMenu)
-
-const items = Array.from(document.querySelectorAll('dropdown__item'))
+const items = Array.from(document.querySelectorAll('.dropdown__link'))
 items.forEach((item) => {
     item.addEventListener('click', () => {
-        document.querySelector('.dropdown__value').textContent = item.querySelector('.dropdown__link').textContent
-        closeMenu()
+        document.querySelector('.dropdown__value').textContent = item.textContent
+        list.classList.remove('dropdown__list_active')
+        return
     })
 })
